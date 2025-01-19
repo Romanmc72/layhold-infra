97a66bcdede1ddcc8977708da3758d2a37777ef7
Roman Czerwinski <romanczerwinski@r0m4n.com>
2025-01-19 16:05:10 -0600

    holy moly it works

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,6 @@ export const IMAGE_NAME = 'app';
 
 /** The name for the app that this code applies to. */
 export const APP_NAME = 'layhold';
+
+/** The name of the github repo containing the image to build. */
+export const IMAGE_REPO = `${APP_NAME}/rails-app`;
